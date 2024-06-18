889a8603fa5bcfac4b860ea3407808b89934815d
josuafrino <josuafrino@gmail.com>
2024-06-18 15:20:13 +0700

    Your commit message

--- Project20-4-ForumDiskusiApp-Frontend/src/App.jsx
+++ Project20-4-ForumDiskusiApp-Frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import "./App.css";
-import { SearchBar } from "./components/bar/SearchBar";
-import { SearchResultsList } from "./components/bar/SearchResultsList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SearchBar } from "./components/molecules/Search/SearchBar";
+import { SearchResultsList } from "./components/molecules/Search/SearchResultsList";
 
 function App() {
   const [results, setResults] = useState([]);
 
   return (
-    <div className="App">
-      <div className="search-bar-container">
-        <SearchBar setResults={setResults} />
+    <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh", backgroundColor: "#eee" }}>
+      <div className="w-50 min-vw-25 d-flex flex-column align-items-center">
+        <div className="mt-5 w-100">
+          <SearchBar setResults={setResults} />
+        </div>
         {results && results.length > 0 && <SearchResultsList results={results} />}
       </div>
     </div>
